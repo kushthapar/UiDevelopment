fbbe61ad26695cb63524653480b831cebd96726f
kushthapar <53950223+kushthapar@users.noreply.github.com>
2019-10-05 10:54:25 -0400

    Updated code

--- OnlinePosts/src/pages/postdetails/postdetails.ts
+++ OnlinePosts/src/pages/postdetails/postdetails.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
+import { UserDetailsService } from '../../app/user.details.service';
+import { PostDetailService } from '../../app/post.details.service';
 
 @Component({
   selector: 'page-postdetails',
   templateUrl: 'postdetails.html'
 })
-export class PostDetails implements OnInit {
+export class PostDetails {
 
   title: any;
   desc: any;
@@ -15,35 +17,36 @@ export class PostDetails implements OnInit {
 
   userRes: any;
   postDetailRes: any;
-  count = 0;
-  userDetailsUrl = "https://jsonplaceholder.typicode.com/users/";
-  postDetailsUrl = "https://jsonplaceholder.typicode.com/posts/";
+  count = 1;
 
-  constructor(public navCtrl: NavController, private http: HttpClient, private navParams: NavParams) {
+  constructor(public navCtrl: NavController, private http: HttpClient, private navParams: NavParams,
+     private userdetailservice: UserDetailsService, private postdetailservice: PostDetailService) {
     this.title = navParams.get('title');
     this.desc = navParams.get('description');
     this.userid = navParams.get('id');
     this.postid = navParams.get('postid');
     
-    console.log(this.postid);
+    this.userdetailservice.userid = this.userid;
+    this.postdetailservice.postid = this.postid;
+
+    this.getUserDetails();
+    this.getPostDetails();
   }
 
-  ngOnInit() {
-    this.userDetailsUrl = this.userDetailsUrl + this.userid
-    this.postDetailsUrl = this.postDetailsUrl + this.postid + "/comments";
-    
-    this.http.get(this.userDetailsUrl).subscribe(data => {
+  getUserDetails() {
+    this.userdetailservice.getUserDetails().subscribe(data => {
       this.userRes = data;
     });
+  }
 
-    this.http.get(this.postDetailsUrl).subscribe(data => {
+  getPostDetails() {
+    this.postdetailservice.getPostDetail().subscribe(data => {
       this.postDetailRes = data;
 
       for(var i = 0; i < this.postDetailRes.length; i++) {
         if(this.postDetailRes[i].postId == this.postid)
-          // console.log(this.postDetailRes[i].postId + "/" + this.postid)
           this.count++;
       }
     });
-  }
+  } 
 }
